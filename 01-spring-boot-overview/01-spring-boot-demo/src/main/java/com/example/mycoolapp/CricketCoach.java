2d6ea00fc16fb9600be0a171dc5f9e0d46407aae
Divante Parness <Divante111@gmail.com>
2025-06-14 19:04:01 -0500

    updated cricket coach

--- 01-spring-boot-overview/01-spring-boot-demo/src/main/java/com/example/mycoolapp/CricketCoach.java
+++ 01-spring-boot-overview/01-spring-boot-demo/src/main/java/com/example/mycoolapp/CricketCoach.java
@@ -9,6 +9,6 @@ public class CricketCoach implements Coach{
     // Must overide the classes needed for implementation in the superclass
     @Override
     public String getDailyWorkout(){
-        return "Practice fast bowling for 15 minutes";
+        return "Practice fast bowling for 15 minutes!";
     }
 }

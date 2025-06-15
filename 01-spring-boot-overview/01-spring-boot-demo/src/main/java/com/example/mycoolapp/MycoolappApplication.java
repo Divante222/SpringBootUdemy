b2fa28735c1ec6b6589a13047ece4798a8f1347b
Divante Parness <Divante111@gmail.com>
2025-06-15 16:20:59 -0500

    setup database connection

--- 01-spring-boot-overview/01-spring-boot-demo/src/main/java/com/example/mycoolapp/MycoolappApplication.java
+++ 01-spring-boot-overview/01-spring-boot-demo/src/main/java/com/example/mycoolapp/MycoolappApplication.java
@@ -1,7 +1,9 @@
 package com.example.mycoolapp;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 // if you want the project to scan outside of mycoolapp or the base scanning location
 // Add the base packages to the scanBasePackages parameter inside of the SpringBootApplication annotation
@@ -15,6 +17,14 @@ public class MycoolappApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MycoolappApplication.class, args);
+	}
+
+	// Executed after beans have been loaded
+	@Bean
+	public CommandLineRunner commandLineRunner(String[] args){
+		return runner -> {
+			System.out.println("Hello World");
+		};
 	}
 
 }
